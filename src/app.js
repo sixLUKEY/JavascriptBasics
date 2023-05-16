063f0de11a731478ed0413743dcc737ca74316e6
sixLUKEY <marcoslukerojo@gmail.com>
2023-05-16 10:05:28 +0200

    first commit

---
+++ src/app.js
@@ -0,0 +1,48 @@
+function myFunction () {
+    alert("Hello! My name is Luke Evertson!!");
+}
+
+function myFunction2() {
+    alert("Hello World!")
+}
+
+function myFunction3() {
+    alert("This is the browsers alert box!")
+}
+function myFunction4() {
+    alert("IDK bruh use your javascript calculator")
+}
+
+const interestRate = 0.3;
+// interestRate = 1;
+console.log(interestRate);
+
+const person = {name2:'Nadine', age2:19, id:0105045137077};
+let {name2, age2, id} = person;
+console.log(name2, age2, id);
+// console.log(age);
+// console.log(id);
+
+let str = "Hello There!";
+let str2 = 'single quotes work fine'
+let phrase = `can embed "${str}"`;
+console.log(phrase);
+
+let name = 'Luke';
+let age = 21.5;
+let isUser = true;
+let firstName = undefined;
+let selectedColor = null;
+
+console.log(typeof selectedColor);
+
+
+let me = {
+    name: 'Luke',
+    age: 30,
+};
+
+me.name = 'Jen';
+
+console.log(me.name,age);
+
